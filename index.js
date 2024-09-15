2255ba8e6c1450da134e1e0f320a55938ef7106a
Jeremiah <139189150+Jerimonski@users.noreply.github.com>
2024-09-15 03:47:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 
 // Connecting to MongoDB:
 mongoose.set('strictQuery', false)
-
+ // console.log(error.errors.name.message)
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
@@ -26,7 +26,7 @@ app.use(morgan((tokens, req, res) => {
 app.get('/', (req, res) => {
     res.send('<h1>Hello there!</h1>')
 })
-
+ // console.log(error.errors.name.message)
 app.get('/api/persons', (req, res) => {
     Contact.find({}).then(contact => {
         res.json(contact)
